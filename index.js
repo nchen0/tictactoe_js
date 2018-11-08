89a01861b412998e80e2c4d041aab87c76b69876
Nicky Chen <nicky.chen0@gmail.com>
2018-11-07 19:58:13 -0800

    x's and o's

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-let gameboard = [["", "", ""], ["", "", ""], ["", "", ""]];
+let count = 0;
+let arr = ["", "", "", "", "", "", "", "", ""];
+let cells = document.querySelectorAll(".grid");
+cells.forEach(cell => {
+  cell.addEventListener("click", () => {
+    if (count % 2 === 0) {
+      //arr[Number(cell.getAttribute("dataset"))] = "x";
+      //console.log(arr[Number(cell.getAttribute("dataset"))]);
+      if (arr[Number(cell.textContent)] === "") {
+        cell.textContent = "x";
+      }
+    } else {
+      if (arr[Number(cell.textContent)] === "") {
+        cell.textContent = "o";
+      }
+    }
+    count++;
+  });
+});
+
+console.log("arr is: ", arr);
